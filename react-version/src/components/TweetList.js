c5697f09515de1a79562700f53adb92654d16261
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-15 12:53:32 -0500

    refactor tweet data structure

--- react-version/src/components/TweetList.js
+++ react-version/src/components/TweetList.js
@@ -2,28 +2,31 @@ import React from 'react';
 import Tweet from './Tweet';
 
 function TweetList(){
-  const tweet1 = {
-    avatar: "https://i.imgur.com/73hZDYK.png",
-    firstName: "Issac",
-    lastName: "Newton",
-    handle: "@SirIsaac",
-    body: "If I have seen further it is by standing on the shoulders of giants",
-    age: "10 days ago"
-  };
-
-  const tweet2 = {
-    avatar: "https://i.imgur.com/nlhLi3I.png",
-    firstName: "Descartes",
-    lastName: "Descartes",
-    handle: "@rd",
-    body: "Je pense , donc je suis",
-    age: "10 days ago"
-  };
+  const tweetData = {
+    firstTweet: {
+      avatar: "https://i.imgur.com/73hZDYK.png",
+      firstName: "Issac",
+      lastName: "Newton",
+      handle: "@SirIsaac",
+      body: "If I have seen further it is by standing on the shoulders of giants",
+      age: "10 days ago"
+    },
+  
+    secondTweet: {
+      avatar: "https://i.imgur.com/nlhLi3I.png",
+      firstName: "Descartes",
+      lastName: "Descartes",
+      handle: "@rd",
+      body: "Je pense , donc je suis",
+      age: "10 days ago"
+    }
+  }
+  
 
   return (
     <section className="tweets">
-      <Tweet {...tweet1}/>
-      <Tweet {...tweet2}/>
+      <Tweet {...tweetData.firstTweet}/>
+      <Tweet {...tweetData.secondTweet}/>
     </section>
   );
 }
